7e28762313f6728f2940ee341b1729c8b870533a
Hedwige Uhllanda <hedwigeuhllanda@Hedwiges-MacBook-Air.local>
2022-06-11 13:12:03 -0600

    back end code

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
 from flask import Flask,jsonify,request
-
 from helpers.dbhelpers import run_query
 import sys 
 
@@ -9,14 +8,19 @@ app = Flask(__name__)
 @app.get('/api/animals')
 def animal_get():
     # DB select
-    animal_list = []
-    return jsonify(animal_list), 200
-    
-    
-    
+    animal_list = run_query("SELECT * from animal")
+    resp = []
+    for animal in animal_list:
+        an_obj = {}
+        #has to be the same as the data.get("")
+        an_obj['animalId'] = animal[0]
+        an_obj['animalName'] = animal[1]
+        an_obj['imageUrl'] = animal[2]
+        resp.append(an_obj)
+    return jsonify(resp), 200
     
 
-@app.post('/api/animals/')
+@app.post('/api/animals')
 def animal_post():
     data = request.json
     animal_name = data.get('animalName')
@@ -28,12 +32,13 @@ def animal_post():
         return jsonify("missing required argument : 'imageUrl'"),422
     #error checking the actual values for the arguments
     # DB write
-    return jsonify("animal added"), 200
+    run_query("INSERT INTO animal (name, image_url) VALUES (?,?)",
+                [animal_name,image_url] )
+    return jsonify("animal added"), 201
 
 if len(sys.argv) > 1:
     mode = sys.argv[1]
-    print (sys.argv[0])
-    print (sys.argv[1])
+    
 else:
     print("missing requriement argument: testing")
     exit()
