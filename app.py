003e6e9aff7ed2f1bcc9726bdbc0b29df5ef199d
Hedwige Uhllanda <hedwigeuhllanda@Hedwiges-MacBook-Air.local>
2022-06-11 10:37:32 -0600

    implemented endpoints

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 
 from flask import Flask,jsonify,request
 from flask_cors import CORS
+from helpers.dbhelpers import run_query
 import sys 
 
 app = Flask(__name__)
 CORS(app)
-
-@app.get('/api/animalName')
+@app.get('/api/animals')
 def animal_get():
-    params = request.args
-    print(params)
+    # DB select
+    animal_list = []
+    return jsonify(animal_list), 200
+    
+    
+    
+    
+
+@app.post('/api/animals/')
+def animal_post():
+    data = request.json
+    animal_name = data.get('animalName')
+    image_url = data.get('imageUrl')
+    #if animal name is equal to none
+    if not animal_name:
+        return jsonify("missing required argument 'animalName'"),422
+    if not image_url :
+        return jsonify("missing required argument : 'imageUrl'"),422
+    #error checking the actual values for the arguments
+    # DB write
+
+
---
+++ helpers/dbhelpers.py
@@ -0,0 +1,93 @@
+from unittest import result
+import mariadb
+import dbcreds
+
+def conect_db():
+    conn = None
+    cursor = None
+    
+    try:
+        conn = mariadb.connect(
+                        user = dbcreds.user,
+                        password = dbcreds.password,
+                        host = dbcreds.host,
+                        port = dbcreds.port,
+                        database = dbcreds.database
+    )
+        cursor = conn.cursor()
+        return (conn,cursor)
+
+    except mariadb.OperationalError as e:
+        print("got an operational error")
+        if ("access denied" in e.msg):
+            print("failed to log in")
+        disconnect_db(conn, cursor)
+
+
+
+def disconnect_db(conn, cursor):
+    if(cursor != None):
+        cursor.close()
+    if(conn != None):
+        conn.rollback()
+        conn.close()
+
+
+
+def run_query(statement, args=None):
+    
+    try:
+        (conn, cursor) = conect_db()
+        if statement.startswith("SELECT"):
+            cursor.execute(statement,args)
+            result = cursor.fetchall()
+            print("todtal of {} users" .format(cursor.rowcount))
+            return result
+            
+        else:
+            cursor.execute(statement, args)
+            if cursor.rowcount == 1:
+                conn.commit()
+                print("insert sucessful")
+            else:
+                print("failed to instert")
+    
+    except mariadb.OperationalError as e:
+        print("got an operational error")
+        if ("access denied" in e.msg):
+            print("failed to log in")
+
+    except mariadb.IntegrityError as e:
+        if("CONSTRAINT `user_CHECK_username`" in e.msg):
+            print("error, all usernames must start with the letter J")
+        if ("CONSTRAINT `user_CHECK_age`" in e.msg):
+            print("error user is outside of acceptable range")
+    
+            print("intergity error")
+            print(e.msg)
+
+    except mariadb.ProgrammingError as e:
+        if("SQL syntax" in e.msg):
+            print("Apparently you do not know how to program")
+        else:
+            print("got a different programing error")
+            print(e.msg)
+
+    except RuntimeError as e:
+        print("caught a run time error")
+        e.with_traceback
+
+    except Exception as e :
+        print(e.with_traceback)
+    
+
+run_query("INSERT INTO user(username,age) VALUES (?,?)",["jennifer",50])
+
+#update the user whose usernam  is Maria and set the age to 35
+run_query("UPDATE user SET age=? WHERE username=?", [35,"Maria"])
+
+#delete a user under age of 25
+run_query("DELETE from post")
+
+users = run_query("SELECT * from user")
+print(result)
